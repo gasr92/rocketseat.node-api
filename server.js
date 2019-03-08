5b274bd1abb99e8dcb122e4e07aa24f206b68464
GABRIEL SILVEIRA DA ROSA <GASR@tcrg.wilson.sons>
2019-03-08 13:08:49 -0300

    Nodemon installation

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hellow Rocketseat');
+    res.send('Hellow Rocketseat!!!!');
 });
 
 app.listen(3666); // ouvir a porta 3001 do navegador
